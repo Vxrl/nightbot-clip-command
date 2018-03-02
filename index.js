00fbb138a98516a89c26d26f0860d6a371fb453a
Vxrl <10587894+Vxrl@users.noreply.github.com>
2018-03-01 19:14:32 -0600

    3

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
 /*const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -12,14 +17,14 @@ express()
 */
 
 
-const app = express();
+
 
 app.get("/", (req, res) => 
 {
     res.send("Hello");
 });
 
-app.listen(5000, () =>
+app.listen(port, () =>
 {
-    console.log("Listening on port 5000");
+    console.log("Listening on port ${port}");
 });
