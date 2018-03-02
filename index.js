b2c729448c4f9a5c91fde04f42e9d80ffe2286f3
Vxrl <10587894+Vxrl@users.noreply.github.com>
2018-03-02 07:53:20 -0600

    4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,11 +17,11 @@ express()
 */
 
 
+app.set("view engine", "ejs");
 
-
-app.get("/", (req, res) => 
+app.get("/", (req, res) =>
 {
-    res.send("Hello");
+    res.render("pages/index");
 });
 
 app.listen(port, () =>
