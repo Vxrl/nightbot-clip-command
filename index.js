8b000990ff116c975d031c488fe180643f731c00
Vxrl <10587894+Vxrl@users.noreply.github.com>
2018-03-02 11:52:08 -0600

    8

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const request = require("request");
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-dotenv.config();
-
 
 /*const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -64,7 +60,7 @@ app.get("/auth", (req, res) =>
                      "?client_id=" + encodeURIComponent(process.env.TWITCH_CLIENT_ID) +
                      "&client_secret=" + encodeURIComponent(process.env.TWITCH_CLIENT_SECRET) +
                      "&code=" + encodeURIComponent(code) +
-                     "&grant_type=" + encodeURIComponent(process.env.TWITCH_GRANT_TYPE) +
+                     "&grant_type=authorization_code" +
                      "&redirect_uri=" + encodeURIComponent(process.env.TWITCH_REDIRECT_URI)
             }, (err, httpResponse, body) =>
             {
@@ -74,7 +70,25 @@ app.get("/auth", (req, res) =>
                     return;
                 }
 
-                res.send(body);
+                const response = JSON.parse(body);
+
+                request.post(
+                {
+                    url: "https://id.twitch.tv/oauth2/token" +
+                         "?grant_type=refresh_token" +
+                         "&refresh_token=" + encodeURIComponent(response.refresh_token) +
+                         "&client_id=" + encodeURIComponent(process.env.TWITCH_CLIENT_ID) +
+                         "&client_secret=" + encodeURIComponent(process.env.TWITCH_CLIENT_SECRET)
+                }, (err, httpResponse, body) =>
+                {
+                    if (err)
+                    {
+                        res.send("Auth failure: " + err)
+                        return;
+                    }
+
+                    res.send(body);
+                })
             });
         else
             res.render("pages/error",
