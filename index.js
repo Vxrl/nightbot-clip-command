0ac7735462bd64ae82be2cfb620e477ffd301e8b
Vxrl <10587894+Vxrl@users.noreply.github.com>
2018-03-02 11:10:04 -0600

    7

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const request = require("request");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+dotenv.config();
 
 
 /*const path = require('path')
@@ -27,7 +30,62 @@ app.get("/", (req, res) =>
 
 app.get("/auth", (req, res) =>
 {
-    res.render("pages/auth");
+    if (req.query.hasOwnProperty("channel"))
+    {
+        const channel = req.query.channel;
+
+        if (channel)
+            res.redirect("https://id.twitch.tv/oauth2/authorize" +
+                "?client_id=" + encodeURIComponent(process.env.TWITCH_CLIENT_ID) +
+                "&redirect_uri=" + encodeURIComponent(process.env.TWITCH_REDIRECT_URI) +
+                "&response_type=" + encodeURIComponent(process.env.TWITCH_RESPONSE_TYPE) +
+                "&scope=" + encodeURIComponent(process.env.TWITCH_SCOPE) +
+                "&force_verify=" + encodeURIComponent(process.env.TWITCH_FORCE_VERIFY) +
+                "&state=" + encodeURIComponent(JSON.stringify(
+                {
+                    channel: channel
+                }))
+            );
+        else
+            res.render("pages/error",
+            {
+                message: "Channel not specified"
+            });
+    }
+    else if (req.query.hasOwnProperty("code") && req.query.hasOwnProperty("state"))
+    {
+        const code = req.query.code;
+        const state = req.query.state;
+
+        if (code && state)
+            request.post(
+            {
+                url: "https://id.twitch.tv/oauth2/token" +
+                     "?client_id=" + encodeURIComponent(process.env.TWITCH_CLIENT_ID) +
+                     "&client_secret=" + encodeURIComponent(process.env.TWITCH_CLIENT_SECRET) +
+                     "&code=" + encodeURIComponent(code) +
+                     "&grant_type=" + encodeURIComponent(process.env.TWITCH_GRANT_TYPE) +
+                     "&redirect_uri=" + encodeURIComponent(process.env.TWITCH_REDIRECT_URI)
+            }, (err, httpResponse, body) =>
+            {
+                if (err)
+                {
+                    res.send("Auth failure: " + err)
+                    return;
+                }
+
+                res.send(body);
+            });
+        else
+            res.render("pages/error",
+            {
+               message: "Authorization code and state not found"
+            });
+    }
+    else
+    {
+        res.redirect("/");
+    }
 });
 
 app.listen(port, () =>
